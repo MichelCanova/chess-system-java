5ed510ba11115c5cc616367bdbcb3ed0e3dd5952
Michel Canova <michelcanova1@gmail.com>
2023-09-29 18:47:17 -0400

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,7 @@
 package chess;
 
+import javax.swing.border.Border;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -25,6 +27,13 @@ public class ChessMatch {
 		}
 		return mat;
 	}
+	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position=sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source=sourcePosition.toPosition();
